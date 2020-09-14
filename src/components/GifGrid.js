4185339ee62f47341c7d80a80969a7d07859bf47
Jairo Teran <jairoteran6@gmail.com>
2020-09-13 22:56:12 -0500

    Custom Hooks+timeout

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,17 +1,14 @@
-import React, { useState, useEffect, Fragment } from "react";
-import { getGifs } from "../helpers/getGifs";
+import React, { Fragment } from "react";
+import { useFetchGifs } from "../hooks/usefetchGifs";
 import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    getGifs(category).then(setImages);
-  }, [category]);
+  const { data: images, loading } = useFetchGifs(category);
 
   return (
     <Fragment>
       <h3>{category}</h3>
+      {loading && <p>Loading</p>}
       <div className="card-grid">
         <ol>
           {images.map((img) => (
