9ae1d6cf20c0368e5f48597325e3a99e38691a53
Jairo Andres Teran <jairoteran6@gmail.com>
2020-09-29 22:02:57 -0500

    docs agregado

---
+++ docs/precache-manifest.885bd5d4f72a1f9d8dbc220e741ba7ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e61df54c3fe3347c3f780fbb99751c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8fa49d56e3fb90c6039",
+    "url": "/static/css/main.e13a6c2e.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8fa49d56e3fb90c6039",
+    "url": "/static/js/main.b453f9df.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
